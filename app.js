df334cf69ad0ea51ac1f6bb88bfe262c35b12237
Velpula Pushpa <pushpa.velpula@pacteraedge.com>
2023-05-16 19:27:37 +0530

    frontend

---
+++ app.js
@@ -0,0 +1,24 @@
+// document.getElementById("myButton").onclick = function () {
+//     var  un = document.forms["myform"]["username"].value;
+//     var pw = document.forms["myform"]["password"].value;
+//     if (un == "pushpa" && pw == "pushpa1") {
+//         alert('Login successfull');
+//         location.href = "file:///C:/fullstackwork/Resterant-menu/drinks.html?txt=pushpa&pswd=pushpa1";
+//     }
+//     else {
+//         window.alert("login failed");
+//     }
+// }
+function Sweets(){
+    window.location.replace("C:/fullstackwork/Resterant-menu/sweet.html")
+}
+function Drinks(){
+    window.location.replace("file:///C:/fullstackwork/Resterant-menu/drinks.html?txt=pushpa&pswd=pushpa1")
+}
+function Veg(){
+    window.location.replace("C:/fullstackwork/Resterant-menu/sweet.html")
+}
+function NonVeg(){
+    window.location.replace("C:/fullstackwork/Resterant-menu/sweet.html")
+}
+
